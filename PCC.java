8e65a5c52cf935d2273fa5be028f7b46cd04fa95
studiosoftdev <40466018+studiosoftdev@users.noreply.github.com>
2021-11-04 20:24:45 +0000

    v1.5
    Server now accepts multiple connections but clients cant see each others messages

--- PCC.java
+++ PCC.java
@@ -4,19 +4,30 @@ import java.net.*;
 public class PCC{
 
 	public static void main(String[] args){
+		boolean quit = false;
 		System.out.println("Welcome to pepConnect!\nEnter username: ");
 		String uname = System.console().readLine();
 		uname = "[" + uname + "]";
 		try{      
 			Socket s = new Socket("localhost", 8086);
 			DataOutputStream dos = new DataOutputStream(s.getOutputStream()); //use this to send data to server
+			DataInputStream dis = new DataInputStream(new BufferedInputStream(s.getInputStream()));
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //will use this to print out our received server data
-			String msg = System.console().readLine();
+			String msg = uname;
 			dos.writeUTF(uname);  
 			dos.flush();  //send the message
-			while(!msg.equals("!quit")){
-				System.out.print("Type: ");
-				msg = System.console().readLine();
+			while(!quit){
+				if(msg.equals("!quit")){ //dont wanna quit the loop instnatly since we still wanna send the message to the server so it can tell everyone we left
+					quit = true;
+				}
+				else{ //if the user hasnt quit, ask for them to write their message
+					if (dis.available() != 0) {
+        				String message = dis.readUTF();
+        				System.out.println(message);
+    				}
+					System.out.print("Type: ");
+					msg = System.console().readLine();
+				}
 				dos.writeUTF(msg);
 				dos.flush();
 			}
--- PCS.java
+++ PCS.java
@@ -3,11 +3,79 @@ import java.net.*;
 import java.time.LocalDateTime; //add time to msg
 import java.time.format.DateTimeFormatter; //so it doesnt look terrible
 
-public class PCS {
-	public static void main(String[] args){
-		try{  
-			ServerSocket ss = new ServerSocket(8086); //setup our server socket over port 8086
-			Socket s = ss.accept(); //establishes connection   
+public class PCS{
+	ServerSocket ss;
+	boolean serverOn = true;
+
+	PCS () throws InterruptedException{
+		try{
+			ss = new ServerSocket(8086); //setup our server socket over port 8086
+		} catch (Exception e){
+			System.out.println(e);
+		}
+		while(serverOn){
+			try{  
+				Socket s = ss.accept(); //establishes connection   
+				ClientSock t = new ClientSock(s); //start new thread to handle each connection
+				t.start();
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+			Thread.sleep(500);
+		}
+		try{
+			ss.close();
+		}
+		catch(Exception e){
+			System.out.println(e);
+			System.exit(-1);
+		}
+	}
+
+
+	class ClientSock extends Thread{
+		Socket cs; //client socket
+
+		ClientSock(Socket s){
+			cs = s; //takes socket from connection
+		}
+
+		public void run(){
+			try{
+				//do stuff
+				DataInputStream dis = new DataInputStream(cs.getInputStream()); //input data with DIS using the input stream (console)
+				DataOutputStream dos = new DataOutputStream(cs.getOutputStream()); //use this to send data to client
+				String uname = (String) dis.readUTF(); //user should input uname as first prompt
+				String msg = "";
+				System.out.println(uname + " has joined.");
+				while(true){
+					msg = (String) dis.readUTF();
+					if(msg.length() != 0){
+						if(!(msg.equals("!quit"))){
+							System.out.println("[" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")) + "] " + uname + ": " + msg);
+							dos.writeUTF("[" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")) + "] " + uname + ": " + msg);
+							dos.flush();
+						}
+						else{
+							System.out.println("[" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")) + "] " + uname + " has left.");
+							dos.writeUTF("[" + LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss")) + "] " + uname + " has left.");
+							dos.flush();
+							break;
+						}
+					}
+				} 
+				dis.close();
+				cs.close();
+			} catch (Exception e){
+				System.out.println(e);
+			}
+		}
+	}
+
+	public static void main(String[] args) throws InterruptedException{
+		PCS pcs = new PCS();
+		//ss.close();
+		/*try{
 			DataInputStream dis = new DataInputStream(s.getInputStream()); //input data with DIS using the input stream (console)
 			DataOutputStream dos = new DataOutputStream(s.getOutputStream()); //use this to send data to client
 			String uname = (String) dis.readUTF(); //user should input uname as first prompt
@@ -31,6 +99,6 @@ public class PCS {
 			ss.close();  
 		} catch(Exception e){
 			System.out.println(e);
-		}
+		}*/
 	}
 }
